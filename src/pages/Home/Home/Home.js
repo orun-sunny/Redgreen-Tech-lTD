a25645716eacca722b2a4fb1105269cf9320d409
orun-sunny <sunny.mukut@gmail.com>
2023-06-22 11:40:24 +0600

    heropart

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -24,31 +24,31 @@ export default function Home() {
   let appendNumber = 4;
   let prependNumber = 1;
 
-  const prepend2 = () => {
-    swiperRef.prependSlide([
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-    ]);
-  };
+  // const prepend2 = () => {
+  //   swiperRef.prependSlide([
+  //     '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+  //     '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+  //   ]);
+  // };
 
-  const prepend = () => {
-    swiperRef.prependSlide(
-      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-    );
-  };
+  // const prepend = () => {
+  //   swiperRef.prependSlide(
+  //     '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+  //   );
+  // };
 
-  const append = () => {
-    swiperRef.appendSlide(
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-    );
-  };
+  // const append = () => {
+  //   swiperRef.appendSlide(
+  //     '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+  //   );
+  // };
 
-  const append2 = () => {
-    swiperRef.appendSlide([
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-    ]);
-  };
+  // const append2 = () => {
+  //   swiperRef.appendSlide([
+  //     '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+  //     '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+  //   ]);
+  // };
 
 
 
--- src/pages/Shared/BottomNav/BottomNav.js
+++ src/pages/Shared/BottomNav/BottomNav.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Hero from "../../../assests/GemibookXPro-a1_01.jpg"
 
+
 const BottomNav = () => {
     return (
-        <div>
+        <>
             <div className="">
 
                 <div className="text-center">
@@ -13,21 +14,149 @@ const BottomNav = () => {
             </div>
 
             <div className="hero bg-base-200">
-                <div className="">
-                    <div className='text-center'>
-                        <h1 className="text-5xl font-bold">GemiBook XPro</h1>
-                        <p className="py-2">Stay Productive And Entertained Every Day.</p>
-               
+
+                <div className="swiper mySwiper">
+                    <div className="swiper-wrapper">
+                        <div className="swiper-slide">
+
+                            <div className="relative w-full">
+
+                                <div>
+                                    <img alt='header' className="sm:w-full w-full transition duration-500 hover:scale-125 "
+                                        src={Hero} />
+                                </div>
+
+
+
+                                <p className="absolute tracking-wider  bottom-24 left-36 text-white text-center font-bold md:text-xl ">
+                                    Discover Better
+                                    basic and modern ,
+
+                                    made of steel for your back.</p>
+                            </div>
+
+
+                        </div>
+
+
+
                     </div>
-                   <div>
-                   <img alt='/' src={Hero} className="w-full rounded-lg shadow-2xl" />
-                   </div>
-
-                </div>
-            </div>
+                    <div className="swiper-slide">
 
 
-        </div>
+                        <div className="relative w-full">
+                            <div className="">
+                                <img alt='header' className=" transition duration-500 hover:scale-125 w-full object-cover "
+                                  src={Hero}  />
+                            </div>
+                            <div className=" absolute  bottom-24 left-36 text-white text-center ">
+                                <div className="px-8">
+
+
+                                    <div>
+                                        <p className=" font-archivo, font-bold md:text-xl ">Discover Better basic and modern ,
+                                            {/* </br> */}
+                                            made of steel for your back.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="swiper-slide">
+
+                        <div className="relative w-full">
+                            {/* <div className="">
+                                <img alt='header' className=" transition duration-500 hover:scale-125 w-full object-cover "
+                               src={Hero2} />
+                            </div> */}
+                            <div className=" absolute  bottom-24 left-36 text-white text-center ">
+                                <div className="px-8">
+
+
+                                    <div>
+                                        <p className=" font-archivo, font-bold md:text-xl ">Discover Better basic and modern ,
+                                            {/* </br> */}
+                                            made of steel for your back.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div >
+                    <div className="swiper-slide">
+                        <div className="relative w-full">
+                            <div className="">
+                                <img alt='header' className=" transition duration-500 hover:scale-125 w-full object-cover "
+                                    src="./assets/orun/header4.webp" />
+                            </div>
+                            <div className=" absolute  bottom-24 left-36 text-white text-center ">
+                                <div className="px-8">
+
+
+                                    <div>
+                                        <p className=" font-archivo, font-bold md:text-xl ">Discover Better basic and modern ,
+                                            {/* </br> */}
+                                            made of steel for your back.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div >
+                    <div className="swiper-slide">
+                        <div className="relative w-full">
+                            <div className="">
+                                <img alt='header' className=" transition duration-500 hover:scale-125 w-full object-cover "
+                                    src="./assets/orun/header4.webp" />
+                            </div>
+                            <div className=" absolute  bottom-24 left-36 text-white text-center ">
+                                <div className="px-8">
+
+
+                                    <div>
+                                        <p className=" font-archivo, font-bold md:text-xl ">Discover Better basic and modern ,
+                                            {/* </br> */}
+                                            made of steel for your back.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div >
+                    <div className="swiper-slide">
+
+                        <div className="relative w-full">
+                            <div className="">
+                                <img alt='header' className=" transition duration-500 hover:scale-125 w-full object-cover "
+                                  src={Hero}  />
+                            </div>
+                            <div className=" absolute  bottom-24 left-36 text-white text-center ">
+                                <div className="px-8">
+
+
+                                    <div>
+                                        <p className=" font-archivo, font-bold md:text-xl ">Discover Better basic and modern ,
+                                            {/* </br> */}
+                                            made of steel for your back.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div >
+
+                </div >
+                {/* <div className=" swiper-button-prev"></div>
+                <div className=" swiper-button-next"></div> */}
+
+
+
+
+            </div >
+
+
+        </>
     )
 }
 
